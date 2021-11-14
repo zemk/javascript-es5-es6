40fb2a0a21695d77923cff9823045c0760628ea6
Vadim <88936621+zemk@users.noreply.github.com>
2021-11-14 23:10:56 +0300

    Json

---
+++ Json/js/main.js
@@ -0,0 +1,23 @@
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', ()=> {
+  let request = new  XMLHttpRequest();
+  // request.open(method, url, async, login, pass);
+  // метод
+  request.open('GET', 'js/current.json');
+  request.setRequestHeader('Content-type', 'application/json; charset-utf-8');
+  request.send();
+  // status, statusText, responseText, readeState
+  request.addEventListener('readystatechange', function(){
+    if (request.readyState === 4 && request.status ==200) {
+      
+      let data =JSON.parse(request.response);
+      inputUsd.value = inputRub.value/ data.usd;
+    }else {
+      inputUsd.value ='Что-то пошло не так!';
+    }
+  });
+
+})
+  
